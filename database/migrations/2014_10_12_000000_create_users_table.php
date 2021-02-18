e36122450ea4c8e4fbac5be778634586f085fc4e
Andreza Batista <andreza.adsb@gmail.com>
2021-02-18 10:40:34 -0300

    Adicionando campos a migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 150);
+            $table->string('surname', 150);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('cpf', 14);
+            $table->string('phone', 20);
+            $table->string('zip_code', 9);
+            $table->string('road', 100);
+            $table->string('district', 100);
+            $table->string('city', 100);
+            $table->string('state', 100);
+            $table->string('number', 50);
+            $table->boolean('terms');
             $table->rememberToken();
             $table->timestamps();
         });

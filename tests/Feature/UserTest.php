44468dacd1c4b66c985e8d07c02d431faef30c4a
AndrezaB97 <andreza.adsb@gmail.com>
2021-02-21 17:35:23 -0300

    Adicionando testes

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,12 +6,37 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Http\Livewire\UserForm;
+use App\Http\Livewire\UserUpdate;
 use App\Models\User;
+use Livewire;
 
 class UserTest extends TestCase
 {
-    public function a_name_is_required()
+    /** @test */
+    public function main_page_contains_user_form_livewire_component()
     {
-        
+        $this->get('user/')
+        ->assertSeeLivewire('user-form');
+    }
+
+    /** @test */
+    public function main_page_contains_user_form_update_livewire_component()
+    {
+        $id = User::factory()->create()['id'];
+        $this->get('user/' . $id)
+        ->assertSeeLivewire('user-update');
+    }
+
+    /** @test */
+    public function user_form_update_sends()
+    {
+        Livewire::test(UserUpdate::class)->set('name', 'Teste')
+        ->set('zip_code', '22222-222')
+        ->set('cpf', '333.333.444-09')
+        ->set('road', 'Rua Teste')
+        ->set('district', 'Teste')
+        ->set('state', 'Teste')
+        ->set('city', 'Teste')
+        ->call('userFormSubmit');
     }
 }
